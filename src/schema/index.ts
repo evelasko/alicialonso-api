17bb7bfbad3d2feddfeadddd597407cc9585eec0
velasko <h.superpotter@gmail.com>
2019-10-31 18:47:22 +0100

    upgrade pakcages

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -32,9 +32,9 @@ const schema = makeSchema({
 
 export default schema
 
-// if (process.env.NODE_ENV === 'nexus') {
-//     setInterval(() => {
-//         console.log('schema: ', schema)
-//         // process.exit()
-//     }, 1500)
-// }
+if (process.env.NODE_ENV === 'nexus') {
+    setInterval(() => {
+        console.log('schema: ', schema)
+        // process.exit()
+    }, 1500)
+}
