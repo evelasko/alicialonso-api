d9259e1cfb98ff58e8389419a7b213e62497947a
velasko <h.superpotter@gmail.com>
2019-12-15 13:30:12 +0100

    terminate redis client upon global test teartdown

--- tests/config/globalTeardown.js
+++ tests/config/globalTeardown.js
@@ -1,4 +1,6 @@
 require("ts-node/register");
+const redisClient = require('../../src/helpers').redisClient
 module.exports = async function() {
+   redisClient.end(true)
    // global.stopServer()
 }
